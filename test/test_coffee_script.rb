68315680ef4676d538f676ad85a72911f3be7b13
Joshua Peek <josh@joshpeek.com>
2010-10-29 12:47:43 -0500

    Update test output for CoffeeScript 0.9.x

--- test/test_coffee_script.rb
+++ test/test_coffee_script.rb
@@ -4,13 +4,13 @@ require 'stringio'
 
 class TestCoffeeScript < Test::Unit::TestCase
   def test_compile
-    assert_equal "(function() {\n  puts('Hello, World!');\n})();\n",
+    assert_equal "(function() {\n  puts('Hello, World!');\n}).call(this);\n",
       CoffeeScript.compile("puts 'Hello, World!'\n")
   end
 
   def test_compile_with_io
     io = StringIO.new("puts 'Hello, World!'\n")
-    assert_equal "(function() {\n  puts('Hello, World!');\n})();\n",
+    assert_equal "(function() {\n  puts('Hello, World!');\n}).call(this);\n",
       CoffeeScript.compile(io)
   end
 
