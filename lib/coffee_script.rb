4929361df951205d35dc21eb5d006a1706c63245
Joshua Peek <josh@joshpeek.com>
2010-10-29 12:59:34 -0500

    Use class << self

--- lib/coffee_script.rb
+++ lib/coffee_script.rb
@@ -1,31 +1,33 @@
 module CoffeeScript
-  def self.locate_coffee_bin
-    out = `which coffee`
-    if $?.success?
-      out.chomp
-    else
-      raise LoadError, "could not find `coffee` in PATH"
+  class << self
+    def locate_coffee_bin
+      out = `which coffee`
+      if $?.success?
+        out.chomp
+      else
+        raise LoadError, "could not find `coffee` in PATH"
+      end
     end
-  end
 
-  def self.coffee_bin
-    @@coffee_bin ||= locate_coffee_bin
-  end
+    def coffee_bin
+      @@coffee_bin ||= locate_coffee_bin
+    end
 
-  def self.coffee_bin=(path)
-    @@coffee_bin = path
-  end
+    def coffee_bin=(path)
+      @@coffee_bin = path
+    end
 
-  # Compile a script (String or IO) to JavaScript.
-  def self.compile(script, options = {})
-    script = script.read if script.respond_to?(:read)
-    command = "#{coffee_bin} -sp"
-    command += " --no-wrap" if options[:no_wrap]
+    # Compile a script (String or IO) to JavaScript.
+    def compile(script, options = {})
+      script = script.read if script.respond_to?(:read)
+      command = "#{coffee_bin} -sp"
+      command += " --no-wrap" if options[:no_wrap]
 
-    IO.popen(command, "w+") do |f|
-      f << script
-      f.close_write
-      f.read
+      IO.popen(command, "w+") do |f|
+        f << script
+        f.close_write
+        f.read
+      end
     end
   end
 end
