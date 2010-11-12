d69b8ade084a8a0e2b32e86ad8b19e8675a42833
Joshua Peek <josh@joshpeek.com>
2010-11-12 12:54:03 -0600

    Shell out to node or jsc directly instead of using coffee bin

--- lib/coffee_script.rb
+++ lib/coffee_script.rb
@@ -1,64 +1,103 @@
+require 'tempfile'
+
 module CoffeeScript
-  class << self
-    def locate_coffee_bin
-      out = `which coffee`
-      if $?.success?
-        out.chomp
-      else
-        raise LoadError, "could not find `coffee` in PATH"
+  module Source
+    def self.read
+      File.read(path)
+    end
+
+    def self.version
+      '0.9.4'
+    end
+
+    def self.bare_option
+      @bare_option ||= read.match(/noWrap/) ? 'noWrap' : 'bare'
+    end
+  end
+
+  module JavaScriptCore
+    BIN = "/System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Resources/jsc"
+
+    class << self
+      def supported?
+        File.exist?(BIN)
+      end
+
+      def compile(script, options = {})
+        options = options[:bare] ? "{#{Source.bare_option} : true}" : "{}"
+
+        f = Tempfile.open("coffee.js")
+        f.puts "load(#{Source.path.inspect});"
+        f.puts "print(CoffeeScript.compile(#{script.inspect}, #{options}));"
+        f.close
+
+        out = `#{BIN} #{f.path}`
+        $?.success? ? out.chomp : nil
+      ensure
+        f.close! if f
       end
     end
+  end
 
-    def coffee_bin
-      @@coffee_bin ||= locate_coffee_bin
+  module Node
+    class << self
+      def supported?
+        `which node`
+        $?.success?
+      end
+
+      def compile(script, options = {})
+        options = options[:bare] ? "{#{Source.bare_option} : true}" : "{}"
+
+        f = Tempfile.open("coffee.js")
+        f.puts Source.read
+        f.puts "console.log(this.CoffeeScript.compile(#{script.inspect}, #{options}));"
+        f.close
+
+        out = `node #{f.path}`
+        $?.success? ? out.chomp : nil
+      ensure
+        f.close! if f
+      end
+    end
+  end
+
+  class << self
+    def engines
+      [Node, JavaScriptCore]
     end
 
-    def coffee_bin=(path)
-      @@coffee_bin = path
+    def engine
+      @@engine ||= detect_engine
+    end
+
+    def engine=(engine)
+      @@engine = engine
     end
 
     def version
-      `#{coffee_bin} --version`[/(\d|\.)+/]
+      Source.version
     end
 
     # Compile a script (String or IO) to JavaScript.
     def compile(script, options = {})
-      args = "-sp"
+      script = script.read if script.respond_to?(:read)
 
-      if options[:wrap] == false ||
-          options.key?(:bare) ||
-          options.key?(:no_wrap)
-        args += " --#{no_wrap_flag}"
+      if options.key?(:bare)
+      elsif options.key?(:wrap)
+        options[:bare] = !options[:wrap]
+      elsif options.key?(:no_wrap)
+        options[:bare] = options[:no_wrap]
+      else
+        options[:bare] = false
       end
 
-      execute_coffee(script, args)
-    end
-
-    # Evaluate a script (String or IO) and return the stdout.
-    # Note: the first argument will be process.argv[3], the second
-    # process.argv[4], etc.
-    def evaluate(script, *args)
-      execute_coffee(script, "-s #{args.join(' ')}")
+      engine.compile(script, options)
     end
 
     private
-      def execute_coffee(script, args)
-        command = "#{coffee_bin} #{args}"
-        script  = script.read if script.respond_to?(:read)
-
-        IO.popen(command, "w+") do |f|
-          f << script
-          f.close_write
-          f.read
-        end
-      end
-
-      def no_wrap_flag
-        if `#{coffee_bin} --help`.lines.grep(/--no-wrap/).any?
-          'no-wrap'
-        else
-          'bare'
-        end
+      def detect_engine
+        engines.detect { |engine| engine.supported? }
       end
   end
 end
--- test/test_coffee_script.rb
+++ test/test_coffee_script.rb
@@ -24,32 +24,23 @@ class TestCoffeeScript < Test::Unit::TestCase
       CoffeeScript.compile("puts 'Hello, World!'\n", :wrap => false)
   end
 
-  def test_compile_with_bare
+  def test_compile_with_bare_true
     assert_equal "puts('Hello, World!');",
       CoffeeScript.compile("puts 'Hello, World!'\n", :bare => true)
   end
 
-  def test_compile_with_no_wrap
+  def test_compile_with_bare_false
+    assert_equal "(function() {\n  puts('Hello, World!');\n}).call(this);\n",
+      CoffeeScript.compile("puts 'Hello, World!'\n", :bare => false)
+  end
+
+  def test_compile_with_no_wrap_true
     assert_equal "puts('Hello, World!');",
       CoffeeScript.compile("puts 'Hello, World!'\n", :no_wrap => true)
   end
 
-  def test_evaluate
-    assert_equal "howdy\n",
-    CoffeeScript.evaluate("require('sys').puts('howdy')")
-  end
-
-  def test_evaluate_with_io
-    io = StringIO.new("puts 'howdy'\n")
-    assert_equal "howdy\n", CoffeeScript.evaluate(io)
-  end
-
-  def test_evaluate_with_args
-    script = <<-EOS
-      require("sys").puts(process.argv[3] + " and " + process.argv[4])
-    EOS
-
-    assert_equal "first and second\n",
-    CoffeeScript.evaluate(script, 'first', 'second')
+  def test_compile_with_no_wrap
+    assert_equal "(function() {\n  puts('Hello, World!');\n}).call(this);\n",
+      CoffeeScript.compile("puts 'Hello, World!'\n", :no_wrap => false)
   end
 end
